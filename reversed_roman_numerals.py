23f2584a7da512d7e40bbf1d2aceb7e09803ddde
Lina Chioma Anaso <linachiomaanaso@Chioma-Personal-MacBook-Air.local>
2025-11-14 16:04:19 +0100

    optimize alternative algorithm

--- reversed_roman_numerals.py
+++ reversed_roman_numerals.py
@@ -1,9 +1,16 @@
-def roman_to_int_reverse(roman: str) -> int:
+def roman_to_int_reverse(s: str) -> int:
 	"""
 	Dictionary mapping Roman numerals to their values
 	:param roman: str
 	:return: total: int
 	"""
 
+	roman_values = {
+		"I": 1,
+	}
+
 	total = 0
 	prev_value = 0
+
+	# Iterate through the Roman numeral string from right to left
+	for numeral in reversed(s.upper()):
