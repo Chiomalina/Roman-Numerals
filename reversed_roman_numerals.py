2be68c6a3fbf331f78a99eb9ce826e5309b7b7c7
Lina Chioma Anaso <linachiomaanaso@Chioma-Personal-MacBook-Air.local>
2025-11-14 16:08:43 +0100

    optimize alternative algorithm

--- reversed_roman_numerals.py
+++ reversed_roman_numerals.py
@@ -7,6 +7,12 @@ def roman_to_int_reverse(s: str) -> int:
 
 	roman_values = {
 		"I": 1,
+		"V": 5,
+		"X": 10,
+		"L": 50,
+		"C": 100,
+		"D": 500,
+		"M": 1000,
 	}
 
 	total = 0
@@ -14,3 +20,18 @@ def roman_to_int_reverse(s: str) -> int:
 
 	# Iterate through the Roman numeral string from right to left
 	for numeral in reversed(s.upper()):
+		current_value = roman_values[numeral]
+
+		# If the current value is greater than or equal to the previous value,
+		# add it to the total (like in "VI = 6)
+		if current_value >= prev_value:
+			total += current_value
+		# If the current value is less than the previous value,
+		#subtract it from the total (like in "IV" = 4)
+		else:
+			total -= current_value
+
+		prev_value = current_value
+
+	return  total
+
